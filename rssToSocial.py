1d991fb71c2dcee2063a5bda413827151ab849f6
Fernando Tricas García <fernand0@elmundoesimperfecto.com>
2020-10-11 17:46:09 +0200

    Improving logging and output

--- rssToSocial.py
+++ rssToSocial.py
@@ -318,10 +318,10 @@ def main():
                         listPosts = blog.getNumPostsData(num, i, lastLink) 
 
                         print("      Scheduling...") 
-                        print("       - Posts: {}".format(post))
+                        #print("       - Posts: {}".format(post))
                         [ print("       - Posts: {}".format(post[0]))
                                 for post in listPosts ]
-                        [ logging.info("    Scheduling posts {}".format(post))
+                        [ logging.info("    Scheduling posts {}".format(post[0]))
                                 for post in listPosts ]
                     if simmulate:
                         print("Simmulation {}".format(str(listPosts))) 
