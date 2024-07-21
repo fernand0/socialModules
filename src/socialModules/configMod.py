0b1f585ac395a7b53ff083a2ebd620fa37521844
Fernando Tricas Garcia <fernand0@elmundoesimperfecto.com>
2024-07-21 19:37:37 +0200

    Improving url management when therere is a page selected

--- src/socialModules/configMod.py
+++ src/socialModules/configMod.py
@@ -49,7 +49,7 @@ def fileNamePath(url, socialNetwork=()):
         myNetloc = f"{myNetloc}_{urlParsed.path[1:]}"
     if myNetloc.endswith('/'):
         myNetloc = myNetloc[:-1]
-    theName = theName.replace('/')
+    myNetloc = myNetloc.replace('/')
     if not socialNetwork:
         theName = (f"{DATADIR}/{myNetloc}")
     else:
