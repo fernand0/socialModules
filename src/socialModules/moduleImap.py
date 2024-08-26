3486c13b6c3bd0dc7df2bfcdf27f65f1e04cb02f
fernand0 <fernand0@elmundoesimperfecto.com>
2024-08-26 17:30:20 +0200

    Setting channel

--- src/socialModules/moduleImap.py
+++ src/socialModules/moduleImap.py
@@ -1379,6 +1379,7 @@ class moduleImap(Content): #, Queue):
         logMsg(msgLog, 2, 0)
         nameF = self.nameFolder(folder)
         logging.debug(f"Folder: {nameF}")
+        self.channel = nameF
         logging.debug(f"Select: {M.select(nameF)}")
         # data = M.sort('ARRIVAL', 'UTF-8', 'ALL')
         if self.getPostsType() == 'new':
