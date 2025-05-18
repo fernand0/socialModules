55585ed434ffd8539ca8113973772470119f9085
Fernando Tricas García <fernand0@elmundoesimperfecto.com>
2025-05-18 23:37:53 +0200

    Body

--- src/socialModules/moduleImap.py
+++ src/socialModules/moduleImap.py
@@ -1309,7 +1309,7 @@ class moduleImap(Content): #, Queue):
                         print(headerToString(msgI['Subject']))
 
     def getPostBody(self, msg):
-        return self.getMessageBody(msg)
+        return self.getMessageBody(msg[1])
 
     def getPostContentHtml(self, msg):
         if isinstance(msg, tuple):
