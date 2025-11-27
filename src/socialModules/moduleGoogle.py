244f936082fc2d37781f62325e0b0cce1c48e2ef
Fernando <fernand0@elmundoesimperfecto.com>
2025-11-27 17:22:17 +0100

    imports

--- src/socialModules/moduleGoogle.py
+++ src/socialModules/moduleGoogle.py
@@ -19,7 +19,9 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.oauth2.credentials import Credentials
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+
+import googleapiclient
+import googleapiclient.errors
 
 
 from socialModules.configMod import *
