c13eebe881b51d1fc276530185788cf6ec03b40a
Fernando <fernand0@elmundoesimperfecto.com>
2025-11-27 16:42:37 +0100

    Adding dependencies

--- src/socialModules/moduleGoogle.py
+++ src/socialModules/moduleGoogle.py
@@ -18,8 +18,8 @@ from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.oauth2.credentials import Credentials
 from google.oauth2 import service_account
-import googleapiclient
 from googleapiclient.discovery import build
+from googleapiclient.errors import httperror
 
 from socialModules.configMod import *
 
