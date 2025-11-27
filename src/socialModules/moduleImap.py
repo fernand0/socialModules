1a3b6318e5705a5519626162d823ec240d956d3b
Fernando <fernand0@elmundoesimperfecto.com>
2025-11-27 10:46:48 +0100

    Debug

--- src/socialModules/moduleImap.py
+++ src/socialModules/moduleImap.py
@@ -1444,6 +1444,8 @@ class moduleImap(Content):  # , Queue):
             self.moveMails(M, messageId, "Trash")
 
     def moveMails(self, M, msgs, folder):
+        logging.info(f"moveMailssss")
+        logging.info(f"moveMailssss {self.channel}")
         if hasattr(self, "channel"):
             M.select(self.channel)
         else:
