b0a3576aa6b9787f8c396b6366f685274e609f61
Fernando <fernand0@elmundoesimperfecto.com>
2025-11-27 10:54:42 +0100

    Debug

--- src/socialModules/moduleImap.py
+++ src/socialModules/moduleImap.py
@@ -1430,6 +1430,7 @@ class moduleImap(Content):  # , Queue):
 
     def deleteApiPosts(self, idPost):
         try:
+            logging.info(f"moveMailssss {self.special}")
             logging.info(f"moveMailssss {self.special['Trash']}")
             res = self.moveMails(
                 self.getClient(), str(idPost).encode(), self.special["Trash"]
