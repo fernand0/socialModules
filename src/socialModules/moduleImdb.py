e3431bb95d97b442fc14629daa96ad68af200c12
Fernando Tricas García <ftricas@unizar.es>
2026-07-31 19:58:30 +0200

    Average

--- src/socialModules/moduleImdb.py
+++ src/socialModules/moduleImdb.py
@@ -264,9 +264,9 @@ class moduleImdb(Content):
             # hini = f"{hini}"
             hfin = self.getPostTimeEnd(post)
             # hfin = f"{hfin.hour}:{hfin.minute}"
-            avg = self.getPostAvg(post)
+            avg = f"{self.getPostAvg(post):.1}"
             line = (
-                    f"> [{avg:.1f}] - "
+                    f"> [{avg}] - "
                 f"({self.getPostCode(post)}) "
                 f"{hini}-{hfin}: "
                 f"{self.getPostTitle(post)}"
